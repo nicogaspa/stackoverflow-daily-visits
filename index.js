24e2f4b20cd1c1681698ff55651004cadeecad67
nicogaspa <nicologasparini94@gmail.com>
2020-01-26 22:34:30 +0100

    selector for login, insert text, submit

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const {chromium, firefox, webkit} = require('playwright');
 
 
 (async () => {
+  const USERNAME = "your_username";
+  const PASSWORD = "your_password";
+
   const browser = await chromium.launch({
     headless:false
   });
@@ -9,6 +12,16 @@ const {chromium, firefox, webkit} = require('playwright');
     viewport:{width: 1000, height: 1000}
   });
   const page = await context.newPage('https://stackoverflow.com/users/login');
-  // await page.screenshot({path: `example.png`});
+  await page.waitFor('#email');
+  const username_selector = await page.$('#email');
+  await username_selector.fill(USERNAME);
+
+  const pass_selector = await page.$('#password');
+  await pass_selector.fill(PASSWORD);
+
+  const submit = await page.$('#submit-button');
+  await submit.click({
+    button:"left"
+  });
   await browser.close();
 })();
