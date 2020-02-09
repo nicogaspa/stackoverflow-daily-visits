c616bce90bd9c47d2d638a93b246555c1c3c430b
nicogaspa <nicologasparini94@gmail.com>
2020-02-09 18:06:28 +0100

    playwright in subfolder, callable index

---
+++ playwright/index.js
@@ -0,0 +1,32 @@
+const {chromium} = require('playwright');
+
+
+(async () => {
+  const USERNAME = "your_username";
+  const PASSWORD = "your_password";
+
+  const browser = await chromium.launch({
+    headless: true
+  });
+  const context = await browser.newContext({
+    viewport: {width: 1000, height: 1000}
+  });
+  const page = await context.newPage('https://stackoverflow.com/users/login');
+  await page.waitFor('#email');
+  const username_selector = await page.$('#email');
+  await username_selector.fill(USERNAME);
+
+  const pass_selector = await page.$('#password');
+  await pass_selector.fill(PASSWORD);
+
+  const submit = await page.$('#submit-button');
+  await submit.click({
+    button: "left"
+  });
+
+  await page.waitFor('.my-profile');
+  const myprofile = await page.$('.my-profile');
+  await myprofile.click();
+
+  await browser.close();
+})();
